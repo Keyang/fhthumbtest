849cceb7d8f012ddbc5482c6ae2906504e6e64a2
keyang <keyang.xiang@feedhenry.com>
2013-07-15 18:46:04 +0100

    added stress test

--- cloud/main.js
+++ cloud/main.js
@@ -1,10 +1,22 @@
 var util = require('util');
 var thumb=require("./thumb.js");
+var async=require("async");
 /* main.js
  * All calls here are publicly exposed as REST API endpoints.
  * - all parameters must be passed in a single JSON paramater.
  * - the return 'callback' method signature is 'callback (error, data)', where 'data' is a JSON object.
 */
+var count=0;
+function _genThumbnailFunc(){
+    return function (cb){
+        count++;
+        console.log("Start time:"+count);
+        thumb.generateFromFilePath(__dirname+"/test.pdf","/tmp/test.png",function(){
+            console.log("finished time:"+count);
+            cb(null,{"done":"ok"});
+        });
+  }
+}
 
 /* 'getConfig' server side REST API method.
  * Trivial example of pulling in a shared config file.
@@ -16,7 +28,14 @@ exports.getConfig = function(params, callback) {
 };
 
 exports.thumbgen=function(params,cb){
-    thumb.generateFromFilePath(__dirname+"/test.pdf","/tmp/test.png",function(){
+    var genThumbnailFunc=[];
+    console.log("called");
+    count=0;
+    for (var i=0;i<100;i++){
+        genThumbnailFunc.push(_genThumbnailFunc());
+    }
+    console.log("ready to go");
+    async.series(genThumbnailFunc,function(){
         cb(null,{"done":"ok"});
     });
 }
